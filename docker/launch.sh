32329db1846a7cdc65e843893cfffbca2f899572
Jinwon Pyo <gch99517@gmail.com>
2025-05-20 01:31:35 +0900

    Declare container name

--- docker/launch.sh
+++ docker/launch.sh
@@ -3,12 +3,14 @@
 # launch.sh가 있는 디렉토리의 부모 = top_directory
 PROJECT_DIR=$(cd "$(dirname "$0")/.." && pwd)
 IMAGE_NAME="ts-zsh-dev"
+CONTAINER_NAME="gomoku-dev"
 
 # Docker 이미지 빌드
 docker build -t $IMAGE_NAME -f "$PROJECT_DIR/docker/Dockerfile" "$PROJECT_DIR"
 
-# 컨테이너 실행 (top_directory를 /gomoku으로 마운트)
+# 컨테이너 실행 (top_directory를 /gomoku 으로 마운트)
 docker run --rm -it \
+  --name $CONTAINER_NAME \
   -v "$PROJECT_DIR":/gomoku \
   -w /gomoku \
   $IMAGE_NAME
