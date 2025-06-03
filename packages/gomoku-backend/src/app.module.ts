54845f16b26142b2c6a687f30299fc1193069519
Jinwon Pyo <gch99517@gmail.com>
2025-06-03 16:41:26 +0000

    Relocate backend source codes to parent directory

--- packages/gomoku-backend/src/app.module.ts
+++ packages/gomoku-backend/src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { SocketIoGateway } from './game/socketio-gateway';
-import { WsGateway } from './game/ws-gateway';
-import { GameService } from './game/game-service';
+import { SocketIoGateway } from './socketio-gateway';
+import { WsGateway } from './ws-gateway';
+import { GameService } from './game-service';
 
-import { RedisService } from './game/redis.service';
+import { RedisService } from './redis.service';
 
 @Module({
   imports: [],
