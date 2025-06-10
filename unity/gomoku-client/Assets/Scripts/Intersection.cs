ff684008f6740d21ea9fac67bfea1ca837a2c6bf
Jinwon Pyo <gch99517@gmail.com>
2025-06-11 01:52:00 +0900

    Destroy previous stone instance

--- unity/gomoku-client/Assets/Scripts/Intersection.cs
+++ unity/gomoku-client/Assets/Scripts/Intersection.cs
@@ -5,6 +5,8 @@ public class Intersection : MonoBehaviour
     [SerializeField]
     private int row_index, col_index; // 15 x 15 보드상에서 교차점의 행과 열 인덱스
 
+    private GameObject stone = null; // 교차점에 놓인 돌 오브젝트
+
     // Update is called once per frame
     private void OnMouseDown()
     {
@@ -33,6 +35,10 @@ public class Intersection : MonoBehaviour
 
     public void SetStone(GameObject stonePrefab)
     {
-        Instantiate(stonePrefab, transform.position, Quaternion.identity);
+        if (stone != null)
+        {
+            Destroy(stone);
+        }
+        stone = Instantiate(stonePrefab, transform.position, Quaternion.identity);
     }
 }
